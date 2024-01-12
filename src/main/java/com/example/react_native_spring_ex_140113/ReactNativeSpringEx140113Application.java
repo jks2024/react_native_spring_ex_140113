bad8a97bdb3fe2382b9d603c7e3bb719ba823206
jks2024 <jks2024@gmail.com>
2024-01-13 00:07:55 +0900

    최초커밋

---
+++ src/main/java/com/example/react_native_spring_ex_140113/ReactNativeSpringEx140113Application.java
@@ -0,0 +1,13 @@
+package com.example.react_native_spring_ex_140113;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactNativeSpringEx140113Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactNativeSpringEx140113Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/react_native_spring_ex_140113/ReactNativeSpringEx140113ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.react_native_spring_ex_140113;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactNativeSpringEx140113ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
